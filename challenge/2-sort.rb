38ee1e2956e154ec388f0261885f95c3880935e4
Lucasleg24 <lucaslegrand.pro@gmail.com>
2025-01-13 10:58:33 +0100

    add modif t2

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -9,7 +9,7 @@ ARGV.each do |arg|
     # skip if not integer
     next if arg !~ /^-?[0-9]+$/
 
-    # convert to integer
+    # convert to integers
     i_arg = arg.to_i
 
     # insert result at the right position
