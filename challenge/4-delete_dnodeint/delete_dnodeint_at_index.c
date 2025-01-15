b04281e160ac57bb6ccced14ec5ea0bc84ea2c1f
Lucasleg24 <lucaslegrand.pro@gmail.com>
2025-01-15 15:45:21 +0100

    test resolve t4

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,37 +11,43 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current;
-    unsigned int p;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
-    if (*head == NULL)
-        return (-1);
-
-    current = *head;
-
-    /* Si l'index est 0, supprime le premier nœud */
-    if (index == 0)
-    {
-        *head = current->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(current);
-        return (1);
-    }
-
-    /* Parcours jusqu'à l'index désiré */
-    for (p = 0; current != NULL && p < index; p++)
-        current = current->next;
-
-    /* Si on n'a pas trouvé l'index */
-    if (current == NULL)
-        return (-1);
-
-    /* Ajuste les pointeurs pour le nœud à supprimer */
-    if (current->next != NULL)
-        current->next->prev = current->prev;
-    current->prev->next = current->next;
-
-    free(current);
-    return (1);
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
+	{
+		*head = (*head)->next;
+		p++;
+	}
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		(*head)->prev->next = (*head)->next;
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		free(*head);
+		*head = saved_head;
+	}
+	return (1);
 }
